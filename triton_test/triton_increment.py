e2c5a708cdc9a1ddc8856d380a001ca12f289ac7
Tom Zheng <tomzheng77@gmail.com>
2025-03-13 21:24:03 +1100

    special kernel for bf16

--- triton_test/triton_increment.py
+++ triton_test/triton_increment.py
@@ -120,8 +120,8 @@ def fused_dequantize_kernel(
     # tl.store(out_offsets1, pid * packed_block_size + tl.arange(0, packed_block_size))
     # tl.store(out_offsets0, values_val0)
     # tl.store(out_offsets1, values_val1)
-    tl.store(out_offsets0, (values_val0 * expanded_absmax_final))
-    tl.store(out_offsets1, (values_val1 * expanded_absmax_final))
+    # tl.store(out_offsets0, (values_val0 * expanded_absmax_final))
+    # tl.store(out_offsets1, (values_val1 * expanded_absmax_final))
     # tl.store(out_offsets0, values_val0 * expanded_absmax_final + expanded_absmax_offset)
     # tl.store(out_offsets1, values_val1 * expanded_absmax_final + expanded_absmax_offset)
 
@@ -131,6 +131,151 @@ def fused_dequantize_kernel(
     #
     # # access absmax_final with subgroup_offsets
     # absmax_final_subgroup = tl.load(absmax_final + subgroup_offsets)
+
+    tl.store(out_offsets0, (values_val0 * expanded_absmax_final))
+    tl.store(out_offsets1, (values_val1 * expanded_absmax_final))
+
+# almost entire duplicate, except with output step changed
+@triton.jit
+def fused_dequantize_kernel_bfloat16(
+    absmax_ptr,        # the absmax values in uint8
+    absmax_code_ptr,   # the code values in float32, each absmax value will point to this initially
+    absmax_scale_ptr,  # [0...absmax_block_size) should be scaled by absmax_scale_ptr[0], etc etc
+    absmax_offset_ptr, # a scalar to add all the absmax values by
+    values_ptr,
+    values_code_ptr,
+    output_ptr,
+    n_elements,
+    absmax_block_size: tl.constexpr,
+    absmax_block_size_step: tl.constexpr,
+    half_values_block_size: tl.constexpr,
+    TRITON_BLOCK_SIZE: tl.constexpr,
+    TRITON_BLOCK_SIZE_step: tl.constexpr,
+    packed_block_size: tl.constexpr,
+    packed_block_size_step: tl.constexpr, # 1/8 of packed_block_size
+):
+    # Program ID: each thread block processes one output block
+    pid = tl.program_id(axis=0)
+    sid = tl.program_id(axis=1)
+
+    # ========== [START REFERENCE ABSMAX IMPLEMENTATION] ==========
+    # absmax_idx = pid * absmax_block_size + tl.arange(0, absmax_block_size)
+    # absmax_val_quantized = tl.load(absmax_ptr + absmax_idx).to(tl.int32)
+    #
+    # # write this to output_ptr, this checks out
+    # tl.store(output_ptr + absmax_idx, absmax_val_quantized)
+    #
+    # # select the code values to complete the first step of dequant
+    # absmax_val = tl.load(absmax_code_ptr + absmax_val_quantized)
+    #
+    # # matches index_select
+    # # tensor([0.0930, 0.1352, 0.0097, ..., -0.1633, 0.0733, 0.1773], device='cuda:0')
+    # # tl.store(output_ptr + absmax_idx, absmax_val)
+    #
+    # absmax_scale = tl.load(absmax_scale_ptr + pid)
+    # absmax_offset = tl.load(absmax_offset_ptr) # TODO maybe use constant
+    # absmax_final = absmax_val * absmax_scale + absmax_offset
+    #
+    # # assuming this is correct
+    # # tensor([0.0220, 0.0221, 0.0212, ..., 0.0221, 0.0210, 0.0220], device='cuda:0')
+    # # tensor([0.0220, 0.0221, 0.0212, ..., 0.0221, 0.0210, 0.0220], device='cuda:0')
+    # # tl.store(output_ptr + absmax_idx, absmax_final)
+    # ========== [END REFERENCE ABSMAX IMPLEMENTATION] ==========
+
+    # assume half_values_block_size is 128, this means that
+    # subgroup_offsets = [0, 0, 0, ... (128 times) 1, 1, 1, ... 2, 2, 2, ... 3, 3, 3, ...]
+    subgroup_offsets = tl.arange(0, packed_block_size_step) // half_values_block_size
+
+    # tl.store(output_ptr + pid * TRITON_BLOCK_SIZE + tl.arange(0, packed_block_size), subgroup_offsets)
+
+    expanded_absmax_idx = pid * absmax_block_size + sid * absmax_block_size_step + subgroup_offsets
+    expanded_absmax_idx_quantized = tl.load(absmax_ptr + expanded_absmax_idx).to(tl.int32)
+    expanded_absmax_val = tl.load(absmax_code_ptr + expanded_absmax_idx_quantized)
+    expanded_absmax_scale = tl.load(absmax_scale_ptr + pid)
+    expanded_absmax_offset = tl.load(absmax_offset_ptr) # TODO maybe use constant
+
+    expanded_absmax_intermediate = (expanded_absmax_val * expanded_absmax_scale)
+
+    # TODO remove this flush, it is needed for some unknown reason
+    # TODO likely because it influences the PTX
+    tl.store(
+        output_ptr + pid * TRITON_BLOCK_SIZE + sid * TRITON_BLOCK_SIZE_step + tl.arange(0, packed_block_size_step),
+        expanded_absmax_intermediate,
+        mask=(tl.arange(0, packed_block_size_step) < 1), # NOTE: after sid, also needs to write a single value
+    )
+
+    # use explicit ASM to avoid fusing the add with the mul, which results in a fma, which clobbers precision
+    # expanded_absmax_final = expanded_absmax_intermediate + expanded_absmax_offset
+    expanded_absmax_final = tl.inline_asm_elementwise(
+        asm="add.f32 $0, $1, $2;",
+        constraints="=f,f,f",
+        args=[expanded_absmax_intermediate, expanded_absmax_offset],
+        dtype=tl.float32,
+        is_pure=True,
+        pack=1,
+    )
+
+    # tl.store(output_ptr + pid * TRITON_BLOCK_SIZE + tl.arange(0, packed_block_size), expanded_absmax_final)
+
+    # now lets just write something, anything to the values
+    values_idx = pid * packed_block_size + sid * packed_block_size_step + tl.arange(0, packed_block_size_step)
+    values_val_packed = tl.load(values_ptr + values_idx).to(tl.int32)
+
+    val0 = (values_val_packed >> 4).to(tl.int32)  # High 4 bits
+    val1 = (values_val_packed & 0b1111).to(tl.int32)  # Low 4 bits
+
+    values_val0 = tl.load(values_code_ptr + val0)
+    values_val1 = tl.load(values_code_ptr + val1)
+
+    output_base = output_ptr + pid * TRITON_BLOCK_SIZE + sid * TRITON_BLOCK_SIZE_step
+    out_offsets0 = output_base + 2 * tl.arange(0, packed_block_size_step)
+    out_offsets1 = out_offsets0 + 1
+
+    # at least this populates everything
+    # tensor([[-0.1848, 0.5626, -0.2844, ..., 0.7230, -0.1848, 0.1609],
+    #         [0.1609, 0.1609, 0.3379, ..., 0.4407, -1.0000, -0.6962],
+    #         [0.2461, 0.5626, -0.0911, ..., -0.6962, 0.5626, -0.6962],
+    #         ...,
+    #         [0.7230, 0.1609, 0.3379, ..., -0.2844, 0.3379, 0.5626],
+    #         [-0.5251, 0.3379, -1.0000, ..., 0.4407, -0.6962, 0.0796],
+    #         [0.3379, 0.7230, -0.0911, ..., -0.5251, 0.7230, 0.7230]],
+    #        device='cuda:0')
+    # tl.store(out_offsets0, pid * packed_block_size + tl.arange(0, packed_block_size))
+    # tl.store(out_offsets1, pid * packed_block_size + tl.arange(0, packed_block_size))
+    # tl.store(out_offsets0, values_val0)
+    # tl.store(out_offsets1, values_val1)
+    # tl.store(out_offsets0, (values_val0 * expanded_absmax_final))
+    # tl.store(out_offsets1, (values_val1 * expanded_absmax_final))
+    # tl.store(out_offsets0, values_val0 * expanded_absmax_final + expanded_absmax_offset)
+    # tl.store(out_offsets1, values_val1 * expanded_absmax_final + expanded_absmax_offset)
+
+    # assume half_values_block_size is 128, this means that
+    # subgroup_offsets = [0, 0, 0, ... (128 times) 1, 1, 1, ... 2, 2, 2, ... 3, 3, 3, ...]
+    # subgroup_offsets = tl.arange(0, packed_block_size) // half_values_block_size
+    #
+    # # access absmax_final with subgroup_offsets
+    # absmax_final_subgroup = tl.load(absmax_final + subgroup_offsets)
+
+    # workaround to handle bf16 on Tesla T4
+    # tl.store(out_offsets0, (values_val0 * expanded_absmax_final))
+    # tl.store(out_offsets1, (values_val1 * expanded_absmax_final))
+    val0_bits = tl.cast((values_val0 * expanded_absmax_final), tl.uint32, bitcast=True)  # Updated to use tl.bitcast
+    val0_mantissa_bit = (val0_bits & (1 << 16)) != 0
+    val0_round_bit = (val0_bits & (1 << 15)) != 0
+    val0_sticky_bits = (val0_bits & ((1 << 15) - 1)) != 0
+    val0_should_round_up = (val0_round_bit & val0_sticky_bits) | (val0_round_bit & val0_mantissa_bit)
+    val0_bits_adjusted = tl.where(val0_should_round_up, val0_bits + (1 << 16), val0_bits)
+    val0_bf16_bits = (val0_bits_adjusted >> 16) & 0xffff # TODO handle Inf and NaN
+    tl.store(out_offsets0, val0_bf16_bits.to(tl.uint16).to(tl.bfloat16, bitcast=True))
+
+    val1_bits = tl.cast((values_val1 * expanded_absmax_final), tl.uint32, bitcast=True)  # Updated to use tl.bitcast
+    val1_mantissa_bit = (val1_bits & (1 << 16)) != 0
+    val1_round_bit = (val1_bits & (1 << 15)) != 0
+    val1_sticky_bits = (val1_bits & ((1 << 15) - 1)) != 0
+    val1_should_round_up = (val1_round_bit & val1_sticky_bits) | (val1_round_bit & val1_mantissa_bit)
+    val1_bits_adjusted = tl.where(val1_should_round_up, val1_bits + (1 << 16), val1_bits)
+    val1_bf16_bits = (val1_bits_adjusted >> 16) & 0xffff # TODO handle Inf and NaN
+    tl.store(out_offsets1, val1_bf16_bits.to(tl.uint16).to(tl.bfloat16, bitcast=True))
 
 
 def fused_dequantize(A, quant_state):
@@ -171,7 +316,10 @@ def fused_dequantize(A, quant_state):
     absmax_block_size_step = absmax_block_size >> 2
     grid = lambda meta: (triton.cdiv(n_elements, meta['TRITON_BLOCK_SIZE']),4)
 
-    fused_dequantize_kernel[grid](
+    # TODO: only use the special output if < sm_80
+    is_bfloat16 = quant_state.dtype == torch.bfloat16
+    kernel = fused_dequantize_kernel_bfloat16 if is_bfloat16 else fused_dequantize_kernel
+    kernel[grid](
         absmax_ptr,
         absmax_code_ptr,
         absmax_scale_ptr,
