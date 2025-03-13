148694c9da39ebf53a691f560019b075e8876100
Tom Zheng <tomzheng77@gmail.com>
2025-03-13 18:43:03 +1100

    try to torch compile

--- triton_test/triton_randomized_test.py
+++ triton_test/triton_randomized_test.py
@@ -2,14 +2,14 @@ import torch
 from bitsandbytes.nn import Params4bit
 from unsloth.kernels import fast_dequantize
 
-from triton_test.triton_increment import fused_dequantize
+from triton_test.triton_increment import torch_fn
 
 print('Commencing 100000 iterations of randomized testing')
-for i in range(10000):
+for i in range(10):
     torch.manual_seed(i)
-    tensor = torch.randn((256, 64), dtype=torch.float16, device='cuda')
+    tensor = torch.randn((256, 64), dtype=torch.bfloat16, device='cuda')
     weight = Params4bit(tensor, quant_type='nf4').to("cuda")
-    actual = fused_dequantize(weight.data, weight.quant_state)
+    actual = torch_fn(weight.data, weight.quant_state)
     expected = fast_dequantize(weight, weight.quant_state)
     torch.set_printoptions(precision=18)
     torch.set_printoptions(threshold=torch.inf)
