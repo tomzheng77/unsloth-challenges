fc89453099536db29051145b1b490e07efc9ec49
Tom Zheng <tomzheng77@gmail.com>
2025-03-16 16:00:39 +1100

    some progress on E

--- Problem_E.py
+++ Problem_E.py
@@ -20,52 +20,65 @@ def transformation_function(batch, linear, labels):
 
 class MemoryEfficientLinear(torch.autograd.Function):
     @staticmethod
-    def forward(ctx, X, linear, labels, forward_function):
+    # def forward(ctx, X, linear, labels, forward_function):
+    def forward(ctx, *args):
+        X = args[0]
+        forward_function = args[-1]
+        other_params = args[1:-1]
+
         # TODO early exit if there is only one sample or if the number
         # TODO of samples isn't a multiple of two
         n_batch = X.shape[0]
-        # X0, X1 = torch.chunk(X, chunks=2, dim=0)
-        labels_0, labels_1 = torch.chunk(labels, chunks=2, dim=0)
+
+        # chunk all other tensors instead of Parameters and Modules
+        other_params_chunks = []
+        for i in range(len(other_params)):
+            other_param = other_params[i]
+            assert not isinstance(other_param, list) # to prevent confusing downstream logic
+            if isinstance(other_param, torch.Tensor) and not isinstance(other_param, nn.Parameter):
+                other_param = list(torch.chunk(other_param, chunks=2, dim=0))
+            other_params_chunks.append(other_param)
+
+        Z_output = torch.tensor(0, dtype=torch.float64, device=X.device)
         with torch.enable_grad():
-            X0 = X[:n_batch // 2]
-            X1 = X[n_batch // 2:]
-            assert X0.requires_grad
-            assert X1.requires_grad
-            Z0 = forward_function(X0, linear, labels_0)
-            Z1 = forward_function(X1, linear, labels_1)
-            # at some point, realized need to move the `X0 =` and `X1 =` into this block
-            # use this to check if grad is working
-            output = ((Z0.to(torch.float64) + Z1.to(torch.float64)) * 0.5).to(torch.float32)
-        ctx.save_for_backward(X0, X1, Z0, Z1, linear.weight)
-        return output
+            X_chunks = torch.chunk(X, chunks=2, dim=0)
+            tensors_for_backward: list[torch.Tensor] = list(X_chunks)
+            for i in range(len(X_chunks)):
+                X_chunk = X_chunks[i]
+                assert(X_chunk.requires_grad)
+                size_chunk = X_chunk.shape[0]
+                other_params_chunk = []
+                for j in range(len(other_params)):
+                    other_param = other_params_chunks[j]
+                    if isinstance(other_param, list):
+                        other_param = other_param[i]
+                    other_params_chunk.append(other_param)
+
+                Z_chunk = forward_function(X_chunk, *other_params_chunk)
+                Z_output += Z_chunk * size_chunk
+                tensors_for_backward.append(Z_chunk)
+
+            ctx.save_for_backward(*tensors_for_backward)
+            return (Z_output / n_batch).to(torch.float32)
 
     @staticmethod
     def backward(ctx, grad_output):
-        X0, X1, Z0, Z1, linear_weight = ctx.saved_tensors
+        chunk_count = len(ctx.saved_tensors) // 2
+        Xs = ctx.saved_tensors[:chunk_count]
+        Zs = ctx.saved_tensors[chunk_count:]
 
-        # Gradient scaling factor from the mean
-        grad_scale = grad_output * 0.5
-
-        # print('Z0', Z0)
-        # print('X0', X0)
-
-        # Compute gradients w.r.t. X1 from Z0
-        grad_X0 = torch.autograd.grad(Z0, X0, grad_outputs=grad_scale, retain_graph=True)[0]
-
-        # Compute gradients w.r.t. X0 from Z1
-        grad_X1 = torch.autograd.grad(Z1, X1, grad_outputs=grad_scale, retain_graph=True)[0]
-
-        # grad_linear_weight = (
-        #     torch.autograd.grad(Z0, linear_weight, grad_outputs=grad_scale, retain_graph=True)[0] +
-        #     torch.autograd.grad(Z1, linear_weight, grad_outputs=grad_scale, retain_graph=True)[0]
-        # )[0]
-
-        # print('grad_X0', grad_X0)
-        # print('grad_X1', grad_X1)
-        # print('grad_linear_weight', grad_linear_weight)
+        total_batch_size = sum([X.shape[0] for X in Xs])
+        grad_Xs = []
+        for i in range(chunk_count):
+            X = Xs[i]
+            Z = Zs[i]
+            batch_size = X.shape[0]
+            grad_scale = grad_output * (batch_size / total_batch_size)
+            grad_X = torch.autograd.grad(Z, X, grad_outputs=grad_scale, retain_graph=True)[0]
+            grad_Xs.append(grad_X)
 
         # Assemble full gradient w.r.t. X
-        grad_X = torch.cat([grad_X0, grad_X1], dim=0)
+        grad_X = torch.cat(grad_Xs, dim=0)
 
         # Return gradients for all inputs
         return grad_X, None, None, None
@@ -93,3 +106,43 @@ if __name__ == '__main__':# run tests to see if the outputs match
         assert(torch.allclose(gradI_expected, gradI_actual))
         assert(torch.allclose(gradW_expected, gradW_actual))
         assert(torch.allclose(gradB_expected, gradB_actual))
+
+    # use memory efficient linear for unsloth GRPO
+    import os
+
+    os.environ['UNSLOTH_IS_PRESENT'] = '1'
+    from unsloth_zoo.rl_replacements import UnslothEfficientGRPO
+    import torch
+
+    old_hidden_states = torch.randn(6, 241, 2048, dtype=torch.bfloat16, device="cuda", requires_grad=True)
+    new_hidden_states = old_hidden_states  # don't deviate too much to blow out K-L divergence
+    lm_head = torch.randn(128256, 2048, dtype=torch.bfloat16, device="cuda", requires_grad=True)
+    labels = torch.randint(0, 128256, (6, 240), dtype=torch.int64, device="cuda")
+    completion_input_ids = torch.randint(0, 128256, (6, 240), dtype=torch.int64, device="cuda")
+
+    # filter out 128004
+    # completion_mask = torch.randint(0, 2, (6, 240), dtype=torch.int64, device="cuda")
+    completion_mask = torch.ones_like(completion_input_ids)
+    advantages = torch.randn(6, dtype=torch.float32, device="cuda")
+    # advantages = torch.zeros(6, dtype=torch.float32, device="cuda")
+    beta = 0.04
+    scaler = None
+    n_chunks = 1
+
+    result = UnslothEfficientGRPO.apply(
+        new_hidden_states,
+        old_hidden_states,
+        lm_head,
+        completion_input_ids,
+        completion_mask,
+        advantages,
+        beta,
+        scaler,
+        n_chunks,
+    )
+
+    print(result)
+
+    # my_fn = curried_grpo_function(old_hidden_states, completion_input_ids, completion_mask, advantages, beta)
+    # my_fn(new_hidden_states, lm_head, labels)
+    # loss = MemoryEfficientLinear.apply(new_hidden_states, lm_head, labels, my_fn)
